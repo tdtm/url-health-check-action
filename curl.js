daea707cfed0a0808367507d30415e96eb0d672f
tdtm <tdtm@users.noreply.github.com>
2024-05-03 14:00:41 -0400

    Chore: update versions + apply quality tooling

--- curl.js
+++ curl.js
@@ -1,70 +1,81 @@
-import axios from "axios"
-import * as core from "@actions/core";
+import axios from 'axios'
+import * as core from '@actions/core'
 
 export async function checkURLWithRetry(
-    url, searchString, searchNotString, retries, retryDelay, basicAuthString, followRedirect, retryAll, cookie, useExponentialBackoff
+  url,
+  searchString,
+  searchNotString,
+  retries,
+  retryDelay,
+  basicAuthString,
+  followRedirect,
+  retryAll,
+  cookie,
+  useExponentialBackoff
 ) {
-    let retryCount = 0;
-    let cumulativeDelay = 0;
-    let config = {
-        maxRedirects: followRedirect ? 30 : 0, // set a max to avoid infinite redirects, but that's arbitrary. todo make this an option.
-        headers: {},
-    };
+  let retryCount = 0
+  let cumulativeDelay = 0
+  let config = {
+    maxRedirects: followRedirect ? 30 : 0, // set a max to avoid infinite redirects, but that's arbitrary. todo make this an option.
+    headers: {}
+  }
 
-    if (basicAuthString) {
-        const base64Credentials = Buffer.from(basicAuthString).toString('base64');
-        config.headers.Authorization = `Basic ${base64Credentials}`
-    }
+  if (basicAuthString) {
+    const base64Credentials = Buffer.from(basicAuthString).toString('base64')
+    config.headers.Authorization = `Basic ${base64Credentials}`
+  }
 
-    if (cookie) {
-        config.withCredentials = true
-        config.headers.Cookie = cookie
-    }
+  if (cookie) {
+    config.withCredentials = true
+    config.headers.Cookie = cookie
+  }
 
-    async function makeRequest() {
-        const response = await axios.get(url, config);
-        let passing  = true
+  async function makeRequest() {
+    const response = await axios.get(url, config)
+    let passing = true
 
-        if (response.status === 200) {
-            core.info(`Target ${url} returned a success status code.`);
+    if (response.status === 200) {
+      core.info(`Target ${url} returned a success status code.`)
 
-            if (passing && searchString) {
-                if (!response.data.includes(searchString)) {
-                    core.error(`Target ${url} did not contain the desired string "${searchString}".`);
-                    passing = false;
-                }
-
-                core.info(`Target ${url} did contain the desired string "${searchString}".`);
-            }
-
-            if (passing && searchNotString) {
-                if (response.data.includes(searchNotString)) {
-                    core.error(`Target ${url} did contain the undesired string "${searchNotString}".`);
-                    passing = false;
-                }
-
-                core.info(`Target ${url} did not contain the undesired string "${searchNotString}".`);
-            }
-
-            if (passing) {
-                core.info(`Succeeded after ${retryCount+1} tries (${retryCount} retries), waited ${cumulativeDelay}ms in total.`)
-                return true
-            }
-        } else {
-            core.error(`Target ${url} returned a non-200 status code: ${response.status}`);
+      if (passing && searchString) {
+        if (!response.data.includes(searchString)) {
+          core.error(`Target ${url} did not contain the desired string "${searchString}".`)
+          passing = false
         }
 
-        if (retryCount < retries) {
-            retryCount++;
-            const delay = Math.pow(useExponentialBackoff ? 2 : 1, retryCount) * retryDelay;
-            cumulativeDelay += delay;
-            core.info(`Retrying in ${delay} ms... (Attempt ${retryCount}/${retries})`);
-            await new Promise(resolve => setTimeout(resolve, delay));
-            return makeRequest();
+        core.info(`Target ${url} did contain the desired string "${searchString}".`)
+      }
+
+      if (passing && searchNotString) {
+        if (response.data.includes(searchNotString)) {
+          core.error(`Target ${url} did contain the undesired string "${searchNotString}".`)
+          passing = false
         }
 
-        throw new Error(`Max retries reached.`);
+        core.info(`Target ${url} did not contain the undesired string "${searchNotString}".`)
+      }
+
+      if (passing) {
+        core.info(
+          `Succeeded after ${retryCount + 1} tries (${retryCount} retries), waited ${cumulativeDelay}ms in total.`
+        )
+        return true
+      }
+    } else {
+      core.error(`Target ${url} returned a non-200 status code: ${response.status}`)
     }
 
-    return makeRequest();
+    if (retryCount < retries) {
+      retryCount++
+      const delay = Math.pow(useExponentialBackoff ? 2 : 1, retryCount) * retryDelay
+      cumulativeDelay += delay
+      core.info(`Retrying in ${delay} ms... (Attempt ${retryCount}/${retries})`)
+      await new Promise((resolve) => setTimeout(resolve, delay))
+      return makeRequest()
+    }
+
+    throw new Error(`Max retries reached.`)
+  }
+
+  return makeRequest()
 }
--- index.js
+++ index.js
@@ -1,46 +1,53 @@
-import * as core from "@actions/core";
-import duration from "duration-js";
-import {
-  checkURLWithRetry,
-} from "./curl";
+import * as core from '@actions/core'
+import duration from 'duration-js'
+import { checkURLWithRetry } from './curl'
 
-process.on("unhandledRejection", (reason) => {
+process.on('unhandledRejection', (reason) => {
   if (reason instanceof Error) {
-    core.error(reason.stack); // Because GitHub won't print it otherwise
-    core.setFailed(reason);
+    core.error(reason.stack) // Because GitHub won't print it otherwise
+    core.setFailed(reason)
   } else {
-    core.setFailed(`${reason}`);
+    core.setFailed(`${reason}`)
   }
-});
+})
 
 async function run() {
-  const urlString = core.getInput("url", { required: true });
-  const maxAttemptsString = core.getInput("max-attempts");
-  const retryDelayString = core.getInput("retry-delay");
-  const followRedirect = core.getBooleanInput("follow-redirect");
-  const useExponentialBackoff = core.getBooleanInput("exponential-backoff");
-  const retryAll = core.getBooleanInput("retry-all");
-  const cookie = core.getInput("cookie");
-  const basicAuthString = core.getInput("basic-auth");
-  const searchString = core.getInput("contains");
-  const searchNotString = core.getInput("contains-not");
+  const urlString = core.getInput('url', { required: true })
+  const maxAttemptsString = core.getInput('max-attempts')
+  const retryDelayString = core.getInput('retry-delay')
+  const followRedirect = core.getBooleanInput('follow-redirect')
+  const useExponentialBackoff = core.getBooleanInput('exponential-backoff')
+  const retryAll = core.getBooleanInput('retry-all')
+  const cookie = core.getInput('cookie')
+  const basicAuthString = core.getInput('basic-auth')
+  const searchString = core.getInput('contains')
+  const searchNotString = core.getInput('contains-not')
 
-  const urls = urlString.split("|");
-  const retryDelayMs = duration.parse(retryDelayString).milliseconds();
-  const maxAttempts = parseInt(maxAttemptsString) - 1;
+  const urls = urlString.split('|')
+  const retryDelayMs = duration.parse(retryDelayString).milliseconds()
+  const maxAttempts = parseInt(maxAttemptsString) - 1
 
   for (const url of urls) {
     // We don't need to do it in parallel, we're going to have to
     // wait for all of them anyway
     await checkURLWithRetry(
-        url, searchString, searchNotString, maxAttempts, retryDelayMs, basicAuthString, followRedirect, retryAll, cookie, useExponentialBackoff
-    );
+      url,
+      searchString,
+      searchNotString,
+      maxAttempts,
+      retryDelayMs,
+      basicAuthString,
+      followRedirect,
+      retryAll,
+      cookie,
+      useExponentialBackoff
+    )
   }
 
   // If we reach this without running into an error
-  core.info("All URL checks succeeded.");
+  core.info('All URL checks succeeded.')
 }
 
 run().catch((e) => {
-  core.setFailed(e);
-});
+  core.setFailed(e)
+})
